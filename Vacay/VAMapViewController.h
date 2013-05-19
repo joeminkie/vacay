e98974fd7c564428ed2ea6c1052fabb325b378f8
Joe Minkiewicz <joe.minkie@gmail.com>
2013-05-19 00:00:46 -0400

    Add empty MapView and controller

---
+++ Vacay/VAMapViewController.h
@@ -0,0 +1,15 @@
+//
+//  VAMapViewController.h
+//  Vacay
+//
+//  Created by Joe Minkiewicz on 5/18/13.
+//  Copyright (c) 2013 Beard, Beard & Beard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface VAMapViewController : UIViewController
+
+@end
