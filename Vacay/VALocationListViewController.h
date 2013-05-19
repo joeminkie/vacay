69d371c0ca2ce13b392297e87c616d2bc890ec35
Joe Minkiewicz <joe.minkie@gmail.com>
2013-05-18 22:27:37 -0400

    Add TableVC for locations

---
+++ Vacay/VALocationListViewController.h
@@ -0,0 +1,13 @@
+//
+//  VALocationListViewController.h
+//  Vacay
+//
+//  Created by Joe Minkiewicz on 5/18/13.
+//  Copyright (c) 2013 Beard, Beard & Beard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VALocationListViewController : UITableViewController
+
+@end
